4a89616d53178af0315eb15a60037078cfb15d05
jonyalanoca <jonyalanoca2@gmail.com>
2023-03-12 23:50:54 -0300

    Se agregaron secciones About

---
+++ PorfolioWeb/Areas/Cliente/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PorfolioWeb.Areas.Cliente.Controllers
+{
+    public class AboutController : Controller
+    {
+        [Area("Cliente")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PorfolioWeb/wwwroot/js/site.js
+++ PorfolioWeb/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function MostrarOcultar(id) {
+    var elementoPorId = document.getElementById(id);
+    if (elementoPorId.style.display == 'none') {
+        elementoPorId.style.display = 'block';
+    } else {
+        elementoPorId.style.display = 'none'
+    }
+     
+}
