c785df72a7e169a5d479065846dc470f1354ed79
jonyalanoca <jonyalanoca2@gmail.com>
2023-03-15 22:44:17 -0300

    nuevos cambios

---
+++ PorfolioWeb/Areas/Cliente/Controllers/WorkController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PorfolioWeb.Areas.Cliente.Controllers
+{
+    [Area("Cliente")]
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
